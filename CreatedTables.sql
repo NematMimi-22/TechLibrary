60a6d1e1a29a373e04773e352aa9d64930153bb0
Nemat Mimi <nematmimi01@gmail.com>
2023-09-21 10:01:04 +0300

    Create Tables and Seed

---
+++ CreatedTables.sql
@@ -0,0 +1,31 @@
+use Tech_Library;
+CREATE TABLE Books (
+    BookID INT IDENTITY(1,1) PRIMARY KEY,
+    Title VARCHAR(255),
+    Author VARCHAR(255),
+    ISBN VARCHAR(20),
+    PublishedDate DATE,
+    Genre VARCHAR(50),
+    ShelfLocation VARCHAR(50),
+    CurrentStatus VARCHAR(20) CHECK (CurrentStatus IN ('Available', 'Borrowed'))
+);
+
+CREATE TABLE Borrowers (
+    BorrowerID INT IDENTITY(1,1) PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Email VARCHAR(100),
+    DateOfBirth DATE,
+    MembershipDate DATE
+);
+
+CREATE TABLE Loans (
+    LoanID INT IDENTITY(1,1) PRIMARY KEY,
+    BookID INT,
+    BorrowerID INT,
+    DateBorrowed DATE,
+    DueDate DATE,
+    DateReturned DATE NULL,
+    FOREIGN KEY (BookID) REFERENCES Books (BookID),
+    FOREIGN KEY (BorrowerID) REFERENCES Borrowers (BorrowerID)
+);
