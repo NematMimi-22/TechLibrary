1bf1b53404d3cef6440aa011a15dce1882ff8e42
Nemat Mimi <nematmimi01@gmail.com>
2023-09-23 22:17:27 +0300

    Book Borrowing Frequency Fn

---
+++ fn_BookBorrowingFrequency.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION fn_BookBorrowingFrequency
+(
+    @BookID INT
+)
+RETURNS INT
+AS
+BEGIN
+    DECLARE @BorrowingCount INT;
+    SELECT @BorrowingCount = COUNT(*) FROM Loans WHERE BookID = @BookID;
+    RETURN @BorrowingCount;
+END;
