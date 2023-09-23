a3a457feaa8726a3f0adc6c492f6cb89e5ae0cb6
Nemat Mimi <nematmimi01@gmail.com>
2023-09-23 22:24:40 +0300

    Generate Report Of Books Borrowed Within Specified Date Range

---
+++ Borrowed_Books_Report.sql
@@ -0,0 +1,18 @@
+CREATE PROCEDURE sp_BorrowedBooksReport
+    @StartDate DATE,
+    @EndDate DATE
+AS
+BEGIN
+    SELECT
+        b.Title AS BookTitle,
+        Borrowers.FirstName + ' ' + Borrowers.LastName AS FullName,
+        l.DateBorrowed AS BorrowingDate
+    FROM
+        Loans l
+     JOIN
+        Books b ON l.BookID = b.BookID
+     JOIN
+        Borrowers ON l.BorrowerID = Borrowers.BorrowerID
+    WHERE
+        l.DateBorrowed >= @StartDate AND l.DateBorrowed <= @EndDate;
+END;
