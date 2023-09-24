ba4ad90244c0fba7bda307e93e7dccd725a1bc19
Nemat Mimi <nematmimi01@gmail.com>
2023-09-24 09:45:46 +0300

    Compute Overdue Fees For Given Loan

---
+++ Functions/fn_CalculateOverdueFees.sql
@@ -0,0 +1,21 @@
+CREATE FUNCTION fn_CalculateOverdueFees(@LoanID INT)
+RETURNS int
+AS
+BEGIN
+    DECLARE @OverdueFee int;
+    DECLARE @DueDate DATE;
+    DECLARE @DateReturned DATE;
+	DECLARE @DaysOverdue INT;
+    SELECT @DueDate = DueDate, @DateReturned = DateReturned
+    FROM Loans
+    WHERE LoanID = @LoanID;
+    IF @DateReturned IS NULL
+        SET @DateReturned = GETDATE();
+    SET @DaysOverdue = DATEDIFF(DAY, @DueDate, @DateReturned);
+    IF @DaysOverdue <= 30
+        SET @OverdueFee = @DaysOverdue; 
+    ELSE
+        SET @OverdueFee = 30 + @DaysOverdue * 2.00; 
+
+    RETURN @OverdueFee;
+END;
