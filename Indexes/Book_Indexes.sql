435d3a3092182830c9034f8890dae6a00eaa9981
Nemat Mimi <nematmimi01@gmail.com>
2023-10-02 11:27:08 +0300

    Create Indexes

---
+++ Indexes/Book_Indexes.sql
@@ -0,0 +1,4 @@
+CREATE INDEX IX_Books_Title ON Books (Title);
+CREATE INDEX IX_Books_Author ON Books (Author);
+CREATE INDEX IX_Books_Genre ON Books (Genre);
+CREATE INDEX IX_Books_CurrentStatus ON Books (CurrentStatus);
