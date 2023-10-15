6f6a21ca639c7f76f75e835bba05af2305600c3b
Nemat Mimi <nematmimi01@gmail.com>
2023-10-15 22:08:34 +0300

    Indexes Folder

---
+++ Indexes/Loans_Indexes.sql
@@ -0,0 +1,4 @@
+CREATE INDEX IX_Loans_BookID ON Loans (BookID);
+CREATE INDEX IX_Loans_DateBorrowed ON Loans (DateBorrowed);
+CREATE INDEX IX_Loans_DueDate ON Loans (DueDate);
+CREATE INDEX IX_Loans_DateReturned ON Loans (DateReturned);
